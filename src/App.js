8cf0a801801408c994e0e18521912679554a2876
Joe McCooey <mccooey@hotmail.co.uk>
2024-06-17 20:50:43 +0100

    Tree view added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import {useDropzone} from 'react-dropzone';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import { Treebeard } from 'react-treebeard';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import TextUpdaterNode from './TextUpdaterNode';
 
 import './text-updater-node.css';
+import './App.css';
 
 import ReactFlow, {
   MiniMap,
@@ -45,8 +47,43 @@ const initialEdges = [{ id: 'e1-2', source: '1', target: '2' }];
 const nodeTypes = { textUpdater: TextUpdaterNode };
 
 function App() {
+
+  const initialTreeData = {
+    name: 'root',
+    toggled: true,
+    children: [
+      {
+        name: 'parent',
+        children: [
+          { name: 'child1' },
+          { name: 'child2' }
+        ]
+      },
+      {
+        name: 'loading parent',
+        loading: true,
+        children: []
+      }
+    ]
+  };
+
+  const nodesToTreeData = (nodes) => {
+    return nodes.map((node) => ({
+      name: node.data.label,
+      id: node.id,
+      toggled: true,
+      children: [],
+    }));
+  };
+
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+  const [treeData, setTreeData] = React.useState(nodesToTreeData(nodes));
+  //const treeData = nodesToTreeData(nodes);
+
+  useEffect(() => {
+    setTreeData(nodesToTreeData(nodes));
+  }, [nodes]);
  
   const onConnect = useCallback(
     (params) => setEdges((eds) => addEdge(params, eds)),
@@ -101,10 +138,10 @@ function App() {
   return (
     <div className="App">
       <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
+          <Nav className="navbar-fixed-top">
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
             <NavDropdown title="File" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">New</NavDropdown.Item>
               <NavDropdown.Item {...getRootProps()}><input {...getInputProps()} />Open</NavDropdown.Item>
@@ -117,6 +154,14 @@ function App() {
           </Nav>
         </Navbar.Collapse>
       </Navbar>
+      <div className="App">  {/* Add this line */}
+        <div className="treeview">
+          <Treebeard 
+            data={treeData} 
+            style={treeStyle}
+          />
+        </div>
+
       <div style={{ width: '100vw', height: '100vh' }}>
         <ReactFlow
           nodes={nodes}
@@ -133,8 +178,97 @@ function App() {
           <Background variant="dots" gap={12} size={1} />
         </ReactFlow>
       </div>
+      <div className="flow">
+        <ReactFlow nodes={nodes} edges={initialEdges} style={rfStyle}>
+          <MiniMap />
+          <Controls />
+          <Background />
+        </ReactFlow>
+      </div>
+      </div>
     </div>
   );
 }
+
+
+const treeStyle = {
+  tree: {
+    base: {
+      listStyle: "none",
+      backgroundColor: "white",
+      margin: 0,
+      padding: 0,
+      color: "#00ff00",
+      fontFamily: "lucida grande ,tahoma,verdana,arial,sans-serif",
+      fontSize: "14px",
+      height: "100%",
+      width: "100%",
+    },
+    node: {
+      base: {
+        position: "relative",
+      },
+      link: {
+        cursor: "pointer",
+        position: "relative",
+        padding: "0px 5px",
+        display: "block",
+      },
+      activeLink: {
+        background: "#31363F",
+      },
+      toggle: {
+        base: {
+          position: "relative",
+          display: "inline-block",
+          verticalAlign: "top",
+          marginLeft: "-5px",
+          height: "24px",
+          width: "24px",
+        },
+        wrapper: {
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          margin: "-7px 0 0 -7px",
+          height: "14px",
+        },
+        height: 14,
+        width: 14,
+        arrow: {
+          fill: "#9DA5AB",
+          strokeWidth: 0,
+        },
+      },
+      header: {
+        base: {
+          display: "inline-block",
+          verticalAlign: "top",
+          color: "#9DA5AB",
+        },
+        connector: {
+          width: "2px",
+          height: "12px",
+          borderLeft: "solid 2px black",
+          borderBottom: "solid 2px black",
+          position: "absolute",
+          top: "0px",
+          left: "-21px",
+        },
+        title: {
+          lineHeight: "24px",
+          verticalAlign: "middle",
+        },
+      },
+      subtree: {
+        listStyle: "none",
+        paddingLeft: "19px",
+      },
+      loading: {
+        color: "#E2C089",
+      },
+    },
+  },
+};
 
 export default App;
